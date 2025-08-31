88ee9a07a693487c0b726b3bdcbdb91bc3ee4198
chauhanshantanu2006-alt <chauhanshantanu2006@gmail.com>
2025-09-01 03:21:51 +0530

    Updated

--- positivenegativezero.c
+++ positivenegativezero.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main() {
     int number;
-    printf("Enter an integer: ");
+    printf("Enter an integer");
     scanf("%d", &number);
     if (number > 0) {
         printf("Positive");
@@ -14,4 +14,5 @@ int main() {
         }
     }
     return 0;
+
 }
